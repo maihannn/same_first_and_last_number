a0b751a7c0d584b02100059d1c233cd92c4caafc
maihannn <maicapalillo15@gmail.com>
2024-01-23 13:45:27 +0800

    added pseudocode

---
+++ same_first_and_last_number.py
@@ -0,0 +1,8 @@
+# Name: Jamaica C. Palillo
+# Section: BSCPE 1-5
+
+# Exercise 5: Check if the first and last number of a list is the same.
+
+# Make a list
+
+# Checking if the first and last number is the same
